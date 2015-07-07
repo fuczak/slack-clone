2854fda3df1298664839b0717e6b1a9426a518b7
Damian Prus <prus.damian@gmail.com>
2015-07-07 22:57:01 +0200

    add messaging functionality

---
+++ app/channels/messages.service.js
@@ -0,0 +1,11 @@
+'use strict';
+angular.module('angularfireSlackApp')
+  .factory('Messages', function ($firebaseArray, FirebaseUrl) {
+    var channelMessagesRef = new Firebase(FirebaseUrl + 'channelMessages');
+
+    return {
+      forChannel: function (channelId) {
+        return $firebaseArray(channelMessagesRef.child(channelId));
+      }
+    };
+  });
