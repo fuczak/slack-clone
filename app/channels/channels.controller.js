28ec536b5a717646da62f59aaa631f0474d85c29
Damian Prus <prus.damian@gmail.com>
2015-07-07 22:21:52 +0200

    add create channel function in channels controller

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -8,9 +8,21 @@ angular.module('angularfireSlackApp')
     channelsCtrl.getDisplayName = Users.getDisplayName;
     channelsCtrl.getGravatar = Users.getGravatar;
 
+    channelsCtrl.newChannel = {
+      name: ''
+    };
+
+    channelsCtrl.createChannel = function () {
+      channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function () {
+        channelsCtrl.newChannel = {
+          name: ''
+        };
+      });
+    };
+
     channelsCtrl.logout = function () {
       Auth.$unauth();
       $state.go('home');
     };
-    
+
   });
